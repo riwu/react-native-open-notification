2f3c2f4f79aebf982ee8bb40def744c3e4534882
riwu <me@wangriwu.com>
2018-12-20 11:20:30 +0800

    add compatibility for Android version < 5.0

--- android/src/main/java/com/riwu/notification/open/OpenNotificationModule.java
+++ android/src/main/java/com/riwu/notification/open/OpenNotificationModule.java
@@ -1,6 +1,9 @@
 package com.riwu.notification.open;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
@@ -21,14 +24,20 @@ public class OpenNotificationModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void open() {
-        Intent intent = new Intent();
-        intent.setAction("android.settings.APP_NOTIFICATION_SETTINGS");
-
         ReactContext reactContext = getReactApplicationContext();
         String packageName = reactContext.getPackageName();
-        intent.putExtra("app_package", packageName);
-        intent.putExtra("android.provider.extra.APP_PACKAGE", packageName);
-        intent.putExtra("app_uid", reactContext.getApplicationInfo().uid);
+        Intent intent = new Intent();
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            intent.setAction("android.settings.APP_NOTIFICATION_SETTINGS");
+            intent.putExtra("android.provider.extra.APP_PACKAGE", packageName);
+            intent.putExtra("app_package", packageName);
+            intent.putExtra("app_uid", reactContext.getApplicationInfo().uid);
+        } else {
+            intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+            intent.addCategory(Intent.CATEGORY_DEFAULT);
+            intent.setData(Uri.parse("package:" + packageName));
+        }
 
         getReactApplicationContext().startActivityForResult(intent, REQUEST_CODE, null);
     }
